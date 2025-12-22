62dd95f655a611f816c29df9a01571bffc95ea8f
RSMedicalEmma <esturm@rsmedical.com>
2025-12-22 09:55:13 -0800

    Update doIndexTest.js

--- doIndexTest.js
+++ doIndexTest.js
@@ -1,5 +1,5 @@
 const pageOrder = ['welcome', 'serial', 'patient', 'consent', 'products', 'baseline', 'book', 'activation'];
-const baseURL = 'https://manatee-sprout-4nzt.squarespace.com/test-register';
+const baseURL = 'https://rsmedical.com/test-register';
 const urls = {
     'serial': baseURL + '-new', 
     'patient': baseURL + '-patient-info', 
@@ -52,3 +52,4 @@ function getPostURL(currentPage){
     return postURLs[currentPage] || '';
 
 }
+
