b8b77f5ab878db6d9295d22627b37cfe20a1a974
RSMedicalEmma <esturm@rsmedical.com>
2025-12-22 09:55:35 -0800

    Update DigitalOnboardingTest.js

--- DigitalOnboardingTest.js
+++ DigitalOnboardingTest.js
@@ -1,4 +1,4 @@
-const errorURL = 'https://manatee-sprout-4nzt.squarespace.com/test-register-error';
+const errorURL = 'https://rsmedical.com/test-register-error';
 console.log('Emma - Checking that script is running'); 
 class Visit { //tracks attempts at entering serial
   #attempts;
@@ -217,6 +217,7 @@ window.onclick = function(event) {
     }
 
 };
+
 
 
 
